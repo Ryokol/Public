574d2aa3b082cb918d1287700e2e21591ce604ed
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-10-12 14:25:22 +0200

    new

---
+++ primo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,a,b=o;
+    printf("Scegli un numero: ");
+    scanf("%d",&a);
+    for(i=a;i>0;i--)
+    {
+        if(a/i==a || a%i==0)
+        {
+            b++;
+        }
+    }
+    if(b==2)
+    {
+        printf("%d é un numero primo",a);
+    }
+    else
+    {
+        printf("%d non é un numero primo",a);
+    }
+}
