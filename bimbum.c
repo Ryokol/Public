635c6657ed8125f0214642cacd84860822550446
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-10-18 11:19:50 +0200

    new

--- bimbum.c
+++ bimbum.c
@@ -1,25 +1,25 @@
-#includee<stdio.h>
+#include<stdio.h>
 int main()
 {
     int bim,bum,x,i;
-    printf("Inserisci un numeroda 1-100: ");
+    printf("Inserisci un numero da 1-100: ");
     scanf("%d",&x);
     if(x<=100)
     {
     for(i=1;i<=x;i++)
-    if("i%3==0 && i%5==0")
+    if(i%3==0 && i%5==0)
     {
-        printf("%d è divisibile per 3 e 5 bimbum",i);
+        printf("%d è divisibile per 3 e 5 bimbum\n",i);
     }
     else
     {
     if(i%3==0)
     {
-    printf("%d è divisibile per 3 bim",i);
+    printf("%d è divisibile per 3 bim\n",i);
     }
-    if("i%5==0")
+    if(i%5==0)
     {
-    printf("%d è divisibile per 5 bum",i);
+    printf("%d è divisibile per 5 bum\n",i);
     }
     }
     
