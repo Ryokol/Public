77d3ee33955b8986ccbb174db4ff6a50a835edd5
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-10-19 10:40:28 +0200

    new

---
+++ tartaglia.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j,ns,n=1;
+
+    for(i=1;i<=n,i++)
+    {
+        ns=n-i;  
+    for(j=0;j<ns;j++)   
+    {
+        printf(" ");
+    }
+    for(j=0;j<na;j++)
+    {
+        printf("*");
+    }
+    na=na+2;
+    printf("\n");
+    }
+    
+    
+    
+    )
+
+}
