6d5a4f872cd95a7e3b8a2bd3241510341c681838
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-10-20 11:50:10 +0200

    new

--- string.c
+++ string.c
@@ -10,3 +10,36 @@ int main()
     printf("\n");
     
 }
+
+------------
+#include <stdio.h>
+int main()
+{
+    char c;
+    do
+    {
+    scanf("%c",&c);
+    printf("%c",c);
+    }
+    while(1);
+    printf("\n");
+    
+}
+
+------------
+#include <stdio.h>
+int main()
+{
+    char c;
+    do
+    {
+    scanf("%c",&c);
+    if(c>='a'&&c<='z')
+    {
+    printf("%c",c-('a'-'A'));
+    }
+    }
+    while(c!= '\n');
+    printf("\n");
+    
+}
