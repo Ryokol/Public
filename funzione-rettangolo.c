143ac268171dc5b777ae2481007112a063e8fdec
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-10-25 17:21:58 +0200

    new

--- funzione-rettangolo.c
+++ funzione-rettangolo.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
+
+void riga(int x,y)
+{
+  int i,j;
+  for(j=0;j<y;j++)
+  {
+  for(i=0;i<x;i++)
+  {
+    printf("*");
+  }
+    printf("\n");
+  }
+
+}
 int main()
 {
   int x,y;
   printf("Inserisci 2 numeri: ");
   scanf("%d",&x);
+  riga();
 }
