e5872d755cc56c2422ca510ba58668aac4e2d48d
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-10-27 21:25:55 +0200

    new

--- funzione-amstrong.c
+++ funzione-amstrong.c
@@ -2,31 +2,52 @@
 
 int main()
 {
-    int a,i;
+    int a,b,i;
     for(i=1;i!=0;i++)
     {
         printf("Inserisci un numero: ");
-        scanf("%d",a);
-        potenza(a);
+        scanf("%d",&a);
+        accumulo(a);
         printf("%d",potenza(a));
     }
 }
 
-int potenza(int a)
+int accumulo(int a,int b)
 {
-    int a1,b=1,c,i;
+    int a2,b3,c3=1,j3;
+    for(j3=0;b2!=0;j3++)
+    {
+        a2=a; 
+        a2=a2/c3;
+        if(a2!=0)
+        {
+            a2=c3*10;
+            c3=c3*10:
+        }
+        else(a2==0)
+        {
+            potenza(a,j3);
+        }
+    }
+
+}
+
+int potenza(int a,int j3)
+{
+    int a1,b1=1,i1;
 
     for(i=1;b>0;i*10)
     {
-        a1=(a/i)-((a/(i*10)*10)
+
+        a1=(a/i)-(a/(i*10)*10);
         a1=a-a1;
         if(a1=0)
         {
-            b=0;
+            ;
         }
         else
         {
-            return a1;;
+            return a1;
         }
 
     }
