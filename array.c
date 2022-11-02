0e124067851994f5b59c8ac41ffa2f09a1489772
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-11-02 10:40:02 +0100

    new

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int min(void)
+{
+    int numeri[5];
+        int i;
+            for(i=0;i<5;i++)
+                {
+                        scanf("%d",&numeri[i]);
+                            }
+                                for(i=4;i>=0;i--)
+                                    {
+                                            printf("%d\n",numeri[i]);
+                                                }
+                                                    return 0;
+                                                     }
