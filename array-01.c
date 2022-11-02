8dc50ccaeb7b208bb93fa9e703baaeaaeb154067
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-11-02 11:07:28 +0100

    new

--- array-01.c
+++ array-01.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main(void)
 {
-    int a[DIM]={1.2,2.3,3.4,4.5,5.6};
+    float a[]={1.2,2.3,3.4,4.5,5.6};
     int i;
     for(i=0;i<5;i++)
     {
-        printf("%d\n",a[i]);
+        printf("%f\n",a[i]);
     }
     return 0;
 
