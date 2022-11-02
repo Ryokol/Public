42a0eb1874d98c299c228639b376a374ca53f8e2
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-11-02 12:02:56 +0100

    new

--- array-lista.c
+++ array-lista.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main(void)
 {
-    int a[i];
-    int b;
+    int a[10];
+    int i,n;
+    scanf("%d",&n);
     for(i=0;i<10;i++)
     {
-        scanf("%d",&a);
+        a[i]=i+n+1;
     }
     for(i=0;i<10;i++)
     {
-        printf("%d",a[i]);
+        printf("%d\n",a[i]);
     }
     return 0;
 }
