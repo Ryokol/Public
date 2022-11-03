0f813b928e7e48f513f1de6ac534e138a5000d58
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-11-03 11:08:39 +0100

    new

--- array-trasporto.c
+++ array-trasporto.c
@@ -3,12 +3,19 @@
 
 int main()
 {
-    int a[DIM], b[DIM],i,j=0;
+    int a[DIM], b[DIM],i,dl=0;
     for(i=0;i<DIM,i++)
     {
         scanf("%d",&a[DIM]);
     }
-    for()
+    for(i=0;i<DIM;i++)
+    {
+        if(a[i]>0)
+        {
+            b[dl]=a[i];
+            dl++;
+        }
+    }
 
 }
 //trasferire solo elementii positivi
