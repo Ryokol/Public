201e5540429583324f9f6161821d5c40c8928631
Ryokol <115079795+Ryokol@users.noreply.github.com>
2022-11-03 11:04:29 +0100

    new

---
+++ array-trasporto.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define DIM 5
+
+int main()
+{
+    int a[DIM], b[DIM],i,j=0;
+    for(i=0;i<DIM,i++)
+    {
+        scanf("%d",&a[DIM]);
+    }
+    for()
+
+}
+//trasferire solo elementii positivi
---
+++ array-trovato.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define DIM 5
+
+int main()
+{
+    int a[]={4,3,8,5,1};
+    int reovato = 0,i,elemento;
+    scanf("%d",&elemento);
+    i=0;
+    while (!trovato && i<DIM)
+    {
+        if(a[]...)
+        {
+            ...
+        }
+    }
+
+}
+//trovare un numero dentro al array
